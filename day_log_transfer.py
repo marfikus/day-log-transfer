59fc191ec8ac3279bb9ab96b4be2ef2b760ce05c
Alexey Zaytsev <alex.rv9rh@gmail.com>
2020-07-21 22:22:34 +0500

    Add trim array, write to main log

--- day_log_transfer.py
+++ day_log_transfer.py
@@ -20,3 +20,18 @@ with open(DAY_LOG_WEEK, "r", encoding="utf-8") as day_log_week:
             # если нашли, то запоминаем номер строки:
             last_date_line_num = line_num
         line_num += 1
+
+# print(last_date_line_num)
+# обрезаем список строк с конца до строки с последней датой
+# (убираем последний день):
+last_week = lines[:last_date_line_num - 1]
+# print(last_week)
+# отделяем последний день:
+last_day = lines[last_date_line_num:]
+# print(last_day)
+
+# дописываем список в файл общего лога:
+# with open(DAY_LOG_YEAR, "a", encoding="utf-8") as f:
+with open("test2.txt", "a", encoding="utf-8") as f:
+    f.write("\n\n")
+    f.writelines(last_week)
